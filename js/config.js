6e7c31032276797ace85de4ec97c291dc8c70f01
gwolbers <>
2022-02-19 19:44:01 +0000

    Update from https://github.com/gwolbers/test_workflow/commit/bee2b5768e9dacc2b4dda8e83af2e333bfea1a8e

---
+++ js/config.js
@@ -0,0 +1,36 @@
+var respecConfig =
+{
+  pubDomain: "ow",
+  specStatus: "GN-DEF",
+  specType: "ST",
+  license: "cc-by-nd",
+  publishDate: "2022-01-07",
+  previousPublishDate: "2020-01-07",
+  previousMaturity: "GN-DEF",
+  authors: [
+    {
+      name: "Richard de Graaf",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    },
+    {
+      name: "Nienke Jansen",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    }
+  ],
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    }
+  ],
+  shortName: "IMOW",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ow-IMOW",
+  issueBase: "https://github.com/Geonovum/ow-IMOW/issues",
+  edDraftURI: "https://Geonovum.github.io/ow-IMOW",
+  publishISODate: "2022-01-07T00:00:00.000Z",
+  generatedSubtitle: "Vastgestelde versie 7 januari 2022"
+}
